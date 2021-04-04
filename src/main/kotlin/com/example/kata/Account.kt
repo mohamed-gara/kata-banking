eb67efe1f304a4b7dff8abe4d5336964d7c961c0
Mohamed Gara <mohamed.gara@external.grtgaz.com>
2021-04-04 17:17:42 +0200

    add Account class

---
+++ src/main/kotlin/com/example/kata/Account.kt
@@ -0,0 +1,8 @@
+package com.example.kata
+
+import java.math.BigDecimal
+
+data class Account(
+  private val balance: BigDecimal
+) {
+}
--- src/test/groovy/AccountShould.groovy
+++ src/test/groovy/AccountShould.groovy
@@ -1,10 +1,16 @@
+package com.example.kata
+
 import spock.lang.Specification
 
-class HelloWorldTest extends Specification {
+class AccountShould extends Specification {
+
+    Account sut;
 
     @SuppressWarnings('unused')
     // false positive from IntelliJ for some reason
     def setup() {
-
+        sut = new Account()
     }
+
+
 }
